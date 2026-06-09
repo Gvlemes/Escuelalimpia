a590982bff25a1aca65ee78411a0d1b169d449b8
Gvlemes <eikgames2020@gmail.com>
2026-06-08 19:24:56 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    // Ruta corregida obligatoria para compilar módulos en Qt 6 Móvil
-    const QUrl url(QStringLiteral("qrc:/EscuelaLimpia/main.qml"));
+    const QUrl url(QStringLiteral("qrc:/EscuelaLimpiaApp/main.qml"));
     
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
         &app, [url](QObject *obj, const QUrl &objUrl) {
