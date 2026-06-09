367bae6a4605643950fc93c69e351734382aed24
Gvlemes <eikgames2020@gmail.com>
2026-06-08 22:47:06 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,9 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QSqlDatabase>
-#include <QSqlQuery>
-#include <QSqlError>
-#include <QStandardPaths>
-#include <QDir>
-
-void inicializarBaseDatosMovil() {
-    // Usamos la ruta de caché temporal que Android 14 nunca bloquea
-    QString rutaAlmacenamiento = QStandardPaths::writableLocation(QStandardPaths::CacheLocation);
-    QDir dir(rutaAlmacenamiento);
-    if (!dir.exists()) {
-        dir.mkpath(rutaAlmacenamiento);
-    }
-
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName(dir.filePath("escuela_limpia_movil.db"));
-
-    if (db.open()) {
-        QSqlQuery query;
-        query.exec("CREATE TABLE IF NOT EXISTS reportes ("
-                   "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                   "fecha TEXT, "
-                   "descripcion TEXT, "
-                   "ruta_foto TEXT)");
-    }
-}
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-
-    inicializarBaseDatosMovil();
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/escuelamovil/main.qml"));
